e314913276061dda3c9548fe776471e897cdf307
Ahmed Elemam <ahmed.elemam@its.ws>
2018-04-16 20:34:10 +0200

    Admin module

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/BirthData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/BirthData_.java
@@ -5,21 +5,20 @@ import Entities.Country;
 import Entities.User;
 import java.util.Date;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T23:44:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T20:03:54")
 @StaticMetamodel(BirthData.class)
 public class BirthData_ { 
 
     public static volatile SingularAttribute<BirthData, Date> updateDate;
     public static volatile SingularAttribute<BirthData, String> birthplace;
-    public static volatile CollectionAttribute<BirthData, User> userCollection;
     public static volatile SingularAttribute<BirthData, Integer> id;
     public static volatile SingularAttribute<BirthData, City> cityID;
     public static volatile SingularAttribute<BirthData, String> birthCert;
     public static volatile SingularAttribute<BirthData, Date> birthDate;
+    public static volatile SingularAttribute<BirthData, User> userID;
     public static volatile SingularAttribute<BirthData, Country> countryID;
     public static volatile SingularAttribute<BirthData, Date> createDate;
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/ProfileData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/ProfileData_.java
@@ -3,20 +3,19 @@ package Entities;
 import Entities.User;
 import java.util.Date;
 import javax.annotation.Generated;
-import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-12T23:44:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-16T20:03:54")
 @StaticMetamodel(ProfileData.class)
 public class ProfileData_ { 
 
     public static volatile SingularAttribute<ProfileData, String> picturepath;
     public static volatile SingularAttribute<ProfileData, String> coverimage;
     public static volatile SingularAttribute<ProfileData, Date> updateDate;
-    public static volatile CollectionAttribute<ProfileData, User> userCollection;
     public static volatile SingularAttribute<ProfileData, String> nickname;
     public static volatile SingularAttribute<ProfileData, Integer> id;
+    public static volatile SingularAttribute<ProfileData, User> userID;
     public static volatile SingularAttribute<ProfileData, Date> createDate;
 
 }
