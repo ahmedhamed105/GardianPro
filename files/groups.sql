6d3df86fea6576ff585b51bfa8ed4e3e1a1df840
ahmedibraheem1988 <37977795+ahmedibraheem1988@users.noreply.github.com>
2018-05-03 13:07:08 +0200

    gro

---
+++ files/groups.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM guardianpro.groups
+LIMIT 0, 1000
+
+-- Date: 2018-05-03 10:58
+*/
+INSERT INTO `groups` (`Group_id`,`name`,`AD_name`,`description`,`create_date`,`update_date`) VALUES (1,'admin','AD_Admin','Admin',NULL,NULL);
+INSERT INTO `groups` (`Group_id`,`name`,`AD_name`,`description`,`create_date`,`update_date`) VALUES (2,'user','AD_user','User',NULL,NULL);
+INSERT INTO `groups` (`Group_id`,`name`,`AD_name`,`description`,`create_date`,`update_date`) VALUES (3,'sys','AD_sys','sys',NULL,NULL);
