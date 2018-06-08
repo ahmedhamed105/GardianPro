cd422d6df4dfcac0d1a15094359acb3190453197
Ahmed Elemam <ahmed.elemam@its.ws>
2018-06-08 11:17:45 +0200

    add menu

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/Distinct1_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/Distinct1_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-07T15:34:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-08T11:10:57")
 @StaticMetamodel(Distinct1.class)
 public class Distinct1_ { 
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/PhoneData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/PhoneData_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-07T15:34:23")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-08T11:10:57")
 @StaticMetamodel(PhoneData.class)
 public class PhoneData_ { 
 
