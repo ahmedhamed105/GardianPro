820cce56a168513657d0ac6e1e2a5aaf271e9498
mohammed.ayad <mohammedayad611@gmail.com>
2018-06-20 10:41:00 +0200

    merge

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/Country_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/Country_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-17T12:05:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-19T22:23:27")
 @StaticMetamodel(Country.class)
 public class Country_ { 
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalIdHasMaritalStatusHasAddressPK_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalIdHasMaritalStatusHasAddressPK_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-17T12:05:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-19T22:23:27")
 @StaticMetamodel(NationalIdHasMaritalStatusHasAddressPK.class)
 public class NationalIdHasMaritalStatusHasAddressPK_ { 
 
