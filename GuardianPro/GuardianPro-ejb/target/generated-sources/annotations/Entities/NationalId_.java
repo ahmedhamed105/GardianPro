975e8b69e28273f3c042d16c1c80535ecad832e4
mohammed.ayad <mohammedayad611@gmail.com>
2018-06-20 22:17:00 +0200

    merge 3

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalId_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalId_.java
@@ -11,11 +11,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-20T12:28:29")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-20T21:03:57")
->>>>>>> cfde32bf1179e012d057f8be5c608c1c1fbb2219
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-20T22:02:35")
 @StaticMetamodel(NationalId.class)
 public class NationalId_ { 
 
