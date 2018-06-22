eb79b50d0300bed2035049872c0c0bbc056a96ce
ahmedibraheem1988 <37977795+ahmedibraheem1988@users.noreply.github.com>
2018-06-22 19:29:58 +0200

    commit html chnages to render component names

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/WebsiteData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/WebsiteData_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-20T22:02:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-21T11:09:26")
 @StaticMetamodel(WebsiteData.class)
 public class WebsiteData_ { 
 
