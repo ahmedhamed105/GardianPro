174c8252da65f85b494c1153b962d4fbc0cf2e21
Ahmed Elemam <ahmed.elemam@its.ws>
2018-06-24 13:48:35 +0200

    as

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/SocialData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/SocialData_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-24T11:09:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-24T12:06:55")
 @StaticMetamodel(SocialData.class)
 public class SocialData_ { 
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/UserPassword_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/UserPassword_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-24T11:09:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-24T12:06:55")
 @StaticMetamodel(UserPassword.class)
 public class UserPassword_ { 
 
