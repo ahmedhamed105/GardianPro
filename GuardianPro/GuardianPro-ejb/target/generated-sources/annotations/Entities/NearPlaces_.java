c93e1e8399fd2262505250ba8d7dce5e4e83352b
Ahmed Elemam <ahmed.elemam@its.ws>
2018-06-25 13:10:05 +0200

    fix parts group

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NearPlaces_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NearPlaces_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-25T12:34:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-25T12:56:32")
 @StaticMetamodel(NearPlaces.class)
 public class NearPlaces_ { 
 
