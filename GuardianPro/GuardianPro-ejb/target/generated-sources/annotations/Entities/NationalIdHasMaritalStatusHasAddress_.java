8495fb5c1c9cde349c586593156c195e8f3e4a06
ahmedibraheem1988 <37977795+ahmedibraheem1988@users.noreply.github.com>
2018-06-26 01:18:14 +0200

    component done

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalIdHasMaritalStatusHasAddress_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalIdHasMaritalStatusHasAddress_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-26T01:18:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-26T01:21:18")
 @StaticMetamodel(NationalIdHasMaritalStatusHasAddress.class)
 public class NationalIdHasMaritalStatusHasAddress_ { 
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalId_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/NationalId_.java
@@ -11,7 +11,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-26T01:18:05")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-26T01:21:19")
 @StaticMetamodel(NationalId.class)
 public class NationalId_ { 
 
