ae3affcb585aff71d7363a3eb6e57e7f84c7f378
ahmed hamed <ahmed.elemam@its.ws>
2018-08-18 14:30:56 +0200

    fix1

--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/ProfileData_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/ProfileData_.java
@@ -6,11 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-12T12:29:54")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-11T15:15:06")
->>>>>>> 607240f5272387efdb26ec8ed872c6fb36ba9ac9
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-18T14:18:01")
 @StaticMetamodel(ProfileData.class)
 public class ProfileData_ { 
 
--- GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/SocialDataPK_.java
+++ GuardianPro/GuardianPro-ejb/target/generated-sources/annotations/Entities/SocialDataPK_.java
@@ -4,11 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-12T12:29:54")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-11T15:15:06")
->>>>>>> 607240f5272387efdb26ec8ed872c6fb36ba9ac9
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-08-18T14:18:01")
 @StaticMetamodel(SocialDataPK.class)
 public class SocialDataPK_ { 
 
